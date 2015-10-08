639ad37b05f62fb1087b626bf58683a2fa2aa8e4
Guest User <student@epicodus.com>
2015-10-07 20:49:55 -0700

    Add buttons for changing background color.

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,19 @@ $(document).ready(function() {
 
     event.preventDefault();
   });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+    $("button#red").click(function() {
+      $("body").removeClass();
+      $("body").addClass("red-background");
+  });
 });
